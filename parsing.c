887ac7db3b8a3e680ff00b1873e8b213250df716
dc86htb <dc86_htb@protonmail.com>
2022-05-02 10:27:01 +0200

    fuck segfault

--- parsing.c
+++ parsing.c
@@ -20,40 +20,40 @@ void add_history(char* unused) {}
 
 
 int main(int argc, char** argv) {
-  puts("unlisp Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+  puts("unlisp version 0.0.0.1");
+  puts("PRESS C^c to exit");
   /* Create Some Parsers */
   mpc_parser_t* Number   = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* unlisp    = mpc_new("unlisp");
 
-  /* Define them with the following Language */
+/* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
-  "                                                     \
-    number   : /-?[0-9]+/ ;                             \
-    operator : '+' | '-' | '*' | '/' ;                  \
-    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-    lispy    : /^/ <operator> <expr>+ /$/ ;             \
-  ",
-  Number, Operator, Expr, unlisp);
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, unlisp);
 
   while (1) {
     char* input = readline("unlisp> ");
-		add_history(input);
-		/* Attempt to Parse the user Input */
+	add_history(input);
+	/* Attempt to Parse the user Input */
 	mpc_result_t r;
-		if (mpc_parse("<stdin>", input, unlisp, &r)) {
-		/* On Success Print the AST */
+	if (mpc_parse("<stdin>", input, unlisp, &r)) {
+	/* On Success Print the AST */
 		mpc_ast_print(r.output);
 		mpc_ast_delete(r.output);
-}		else {
+}	else {
 		/* Otherwise Print the Error */
 		mpc_err_print(r.error);
 		mpc_err_delete(r.error);
 }
-    free(input);
+
+	free(input);
   }
-  mpc_cleanup(4, Number, Operator, Expr, unlisp);
   return 0;
 }
