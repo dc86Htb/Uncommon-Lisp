a06ac0b5db07fe0938ad9d3fb1126ceeb4355fe5
dc86htb <dc86_htb@protonmail.com>
2022-05-02 09:17:31 +0200

    edit main

--- main.c
+++ main.c
@@ -21,9 +21,11 @@ int main(int argc, char** argv) {
   puts("Press Ctrl+c to Exit\n");
   while (1) {
     char* input = readline("lispy> ");
-    add_history(input);
-    printf("No you're a %s\n", input);
+		add_history(input);
+		printf("No you're a %s\n", input);
     free(input);
   }
   return 0;
 }
+
+
